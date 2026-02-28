16bf57f86782ef6ef1010f68d9239944bc77db69
Amogh Shendre <amogh.shendre@gmail.com>
2026-02-28 17:40:51 -0600

    data class for ip-api JSON response

--- tv/src/main/java/com/example/ipgeolocator/data/remote/GeolocationResponse.kt
+++ tv/src/main/java/com/example/ipgeolocator/data/remote/GeolocationResponse.kt
@@ -1,4 +1,21 @@
 package com.example.ipgeolocator.data.remote
 
-class GeolocationResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class GeolocationResponse (
+    val status: String,
+    val country: String?,
+    val countryCode: String,
+    val region: String,
+    val regionName: String,
+    val city: String?,
+    val zip: String?,
+    val lat: Double,
+    val lon: Double,
+    val timezone: String,
+    val isp: String?,
+    val org: String?,
+    @SerializedName("as")
+    val asData: String,
+    val query: String,
+)
