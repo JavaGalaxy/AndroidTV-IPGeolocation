e888964fa20b4968c7bbef2409be71aea0c236c4
Amogh Shendre <amogh.shendre@gmail.com>
2026-02-28 17:41:56 -0600

    create retrofit service interface with ip-api.com

--- tv/src/main/java/com/example/ipgeolocator/data/remote/IpApiService.kt
+++ tv/src/main/java/com/example/ipgeolocator/data/remote/IpApiService.kt
@@ -1,4 +1,23 @@
 package com.example.ipgeolocator.data.remote
 
-class IpApiService {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface IpApiService {
+    @GET("json/{ip}")
+    suspend fun getGeolocation(@Path("ip") ipAddress: String): GeolocationResponse
+}
+
+object ApiClient {
+    private const val BASE_URL = "http://ip-api.com/"
+
+    private val retrofit = Retrofit
+                            .Builder()
+                            .baseUrl(BASE_URL)
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build()
+
+    val ipApiService: IpApiService = retrofit.create(IpApiService::class.java)
 }
