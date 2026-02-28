a2d3e82e4bb7766004123d8ebb95c7451703f761
Amogh Shendre <amogh.shendre@gmail.com>
2026-02-28 17:39:49 -0600

    implement room database with singleton pattern

--- tv/src/main/java/com/example/ipgeolocator/data/database/AppDatabase.kt
+++ tv/src/main/java/com/example/ipgeolocator/data/database/AppDatabase.kt
@@ -1,4 +1,41 @@
 package com.example.ipgeolocator.data.database
 
-class AppDatabase {
+import androidx.room.Room
+import android.content.Context
+import com.example.ipgeolocator.data.dao.GeolocationDAO
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.ipgeolocator.data.entity.GeolocationEntity
+
+@Database(
+    entities = [GeolocationEntity::class],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun geolocationDao(): GeolocationDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            if (INSTANCE != null) {
+                return INSTANCE!!
+            } else {
+                synchronized(this) {
+                    if (INSTANCE == null) {
+                        val instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            AppDatabase::class.java,
+                            "geolocation"
+                        ).build()
+                        INSTANCE = instance
+                    }
+                }
+                return INSTANCE!!
+            }
+        }
+    }
 }
